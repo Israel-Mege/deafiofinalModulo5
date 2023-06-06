939ce31b9561aebb39821b89bf502d28c7c942d0
Israel-Mege <israelmege@gmail.com>
2023-06-06 14:37:14 -0400

    se agregan parametros para deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '18.231.42.233', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
